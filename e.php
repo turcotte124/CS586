cce4eef8240244e9e4f37f52c6ae81aaa56ed724
Heath Harrelson <heath.harrelson@gmail.com>
2012-11-21 10:50:52 -0800

    Implement PC Insert Demo
    Implements the PC insertion demo as in Exercise 9.3.1 (e).

--- e.php
+++ e.php
@@ -12,64 +12,94 @@
 
 // import the database connection information
 require_once 'db-config.php';
-$conn = null;
 
-function connect_to_db () {
-	global $conn;
-	global $db_connection_string;
-
-	if (!is_null($conn))
-		return $conn;
-
-	try {
-	    // connect to the database
-		$conn = new PDO($db_connection_string, DB_USER, DB_PASS);
-	} catch (PDOException $e) {
-	  echo "Database exception: " . $e->getMessage() . "\n";
-	  die;
-	}
-
-	return $conn;
+// Attempt to connect to the database
+try {
+	$conn = new PDO($db_connection_string, DB_USER, DB_PASS);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+  echo "Database exception: " . $e->getMessage() . "\n";
+  die;
 }
-
-function disconnect_from_db () {
-	global $conn;
-	$conn = null;
-}
-
-
-/* Entry Point */
 
 // Get the form input
+$maker = isset($_POST['maker']) ? $_POST['maker'] : '';
+$model = isset($_POST['model']) ? $_POST['model'] : '';
+$speed = isset($_POST['speed']) ? $_POST['speed'] : '';
+$ram   = isset($_POST['ram']) ? $_POST['ram'] : '';
+$hd    = isset($_POST['hd']) ? $_POST['hd'] : '';
+$price = isset($_POST['price']) ? $_POST['price'] : '';
 
-// set up manufacturer pull-down list
-	// select distinct maker from product order by maker
-	// how to handle new maker?
+// use the new_maker field instead of the maker field if it's set
+$new_maker = isset($_POST['new_maker']) ? $_POST['new_maker'] : '';
+if (!empty($new_maker)) {
+	$maker = $new_maker;
+}
 
-// get a bunch of stuff from the post array
+$success = false;
+$duplicate = false;
+$transaction_failure = null;
 
-// start a transaction
+// get the list of manufacturers for form's pull-down list
+try {
+	$maker_stmt = $conn->query('SELECT DISTINCT maker FROM product ORDER BY maker');
+	$maker_list = $maker_stmt->fetchAll();
+} catch (PDOException $e) {
+	echo "Database exception: " . $e->getMessage() . "\n";
+	die;
+}
 
-// try to find a product with the given model number
-// select count(model) from product where model = $model_num;
+// form submitted, so try to insert the new product
+if (!empty($model)) {
+	try {
+		$conn->beginTransaction();
 
-// if count is not zero, abort the transaction, report an error
-	// store error message in session
-	// repopulate form with POST data
+		// try to find a product with the given model number
+		$model_stmt = $conn->prepare("SELECT * FROM product WHERE model = :model_num");
+		$model_stmt->bindParam(':model_num', $model);
+		$model_stmt->execute();
 
-// if count is zero
-	// insert data into the product table
-	// insert into product (maker, model, type) values ($maker, $model_num, $type)
-	// fixme: catch exception?
+		// model number already exists
+		if ($model_stmt->rowCount() > 0) {
+			$duplicate = true;
+			$conn->rollBack();
+		}
 
-	// insert data into the pc table
-	// insert into pc (model, speed, ram, hd, price) values ($model_num, $speed, $ram, $hd, $price)
-	// fixme: catch exception?
+		if (!$duplicate) {
+			// add the product to the product table
+			$sql = "INSERT INTO product (model, type, maker) VALUES(:model_num, 'pc', :maker)";
+			$product_insert = $conn->prepare($sql);
+			$product_insert->bindParam(':model_num', $model);
+			$product_insert->bindParam(':maker', $maker);
+			$product_insert->execute();
 
-	// commit transaction
+			// add the product to the pc table
+			$sql = "INSERT INTO pc (model, speed, ram, hd, price) VALUES(?, ?, ?, ?, ?)";
+			$pc_insert = $conn->prepare($sql);
+			$pc_insert->bindParam(1, $model);
+			$pc_insert->bindParam(2, $speed);
+			$pc_insert->bindParam(3, $ram);
+			$pc_insert->bindParam(4, $hd);
+			$pc_insert->bindParam(5, $price);
+			$pc_insert->execute();
 
-	// store success message in session
-	// go back to form
+			$conn->commit();
+
+			// inserts were successful
+			$success = true;
+			$added_model = $model;
+
+			// clear form input
+			$maker = $model = $speed = $ram = $hd = $price = '';		
+		}
+	} catch (PDOException $e) {
+		$transaction_failure = 'Exception in transaction: ' . $e->getMessage();
+	}
+}
+
+// close db connection
+$conn = null;
 ?>
 <!doctype html>
 <html lang="en">
@@ -82,5 +112,57 @@ function disconnect_from_db () {
   <![endif]-->
 </head>
 <body>
+<?php
+	// Report result of form submission
+
+	// transaction was successful
+	if ($success) {
+		printf('<div class="text-success">Added product %s.</div>', $added_model);
+	}
+
+	// duplicate model number found
+	if ($duplicate) {
+		printf('<div class="text-warning">Please change the model number. %s is a duplicate.</div>', $model);
+	}
+
+	// exception occurred in transaction portion
+	if (!empty($transaction_failure)) {
+		printf('<div class="text-failure">%s</div>', $transaction_failure);
+	}
+?>
+
+	<h2>Add a New Product</h2>
+
+	<form action="e.php" method="post">
+		<label for="maker">Manufacturer</label>
+		<select name="maker">
+<?php
+			foreach ($maker_list as $maker_row) {
+				$selected = ($maker_row['maker'] == $maker) ? 'selected="selected"' : '';
+				printf('<option value="%s" %s>%s</option>', $maker_row['maker'], 
+					$selected, $maker_row['maker']);
+			}
+?>
+		</select>
+		<label for="new_maker">New Manufacturer</label>
+		<input type="text" name="new_maker" value="<?php echo $new_maker; ?>"/>
+
+		<label for="model">Model Number</label>
+		<input type="text" name="model" value="<?php echo $model; ?>"/>
+
+		<label for="speed">CPU Speed</label>
+		<input type="text" name="speed" value="<?php echo $speed; ?>"/>
+
+		<label for="ram">RAM in MB</label>
+		<input type="text" name="ram" value="<?php echo $ram; ?>"/>
+
+		<label for="hd">HD Size in GB</label>
+		<input type="text" name="hd" value="<?php echo $hd; ?>"/>
+
+		<label for="price">Price</label>
+		<input type="text" name="price" value="<?php echo $price; ?>"/>
+
+		<input type="submit" value="Add Product"/>
+	</form>
 </body>
 </html>
